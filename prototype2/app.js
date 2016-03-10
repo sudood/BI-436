0a94d7ddeb4fc7a1176d966ab8b21deeafd211b9
sudood <rizzo.mc9@gmail.com>
2016-03-10 14:56:18 -0500

    BI-436 0.1.0 - prototype2 refactoring code

--- prototype2/app.js
+++ prototype2/app.js
@@ -3,14 +3,34 @@ var app = express();
 var server = require('http').Server(app);
 var request = require('request');
 var fs = require("fs");
+var router = express.Router();
+var port = 9000;
+app.use('/api', router);
+app.use(express.static('client'));
+app.get('/*', function(req, res, next){
+  var url = req.url;
+  if(!url.match(/\.\.+?\//)){
+    res.sendFile(req.url, {root:'./client'});
+  } else if(url === '/'){
+    next();
+  } else {
+    res.status(404).send('NOT FOUND');
+  }
+});
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root:'./client'});
+});
+module.exports.listen = function(){
+  server.listen(port, function(){
+    console.log('Server listening on port', port);
+  });
+};
 
-// // For requiring local javascript files.
-
+// FOR REQUIRING LOCAL JAVASCRIPT FILES.
 // var algo = require('./astar.js');
 // var graphDef = require('./graph_definition.js');
 
-// For reading in data from filesystem.
-
+// FOR READING IN DATA FROM FILESYSTEM.
 // fs.readFile('data/coordinates/something.json', 'utf8', function (err,data) {
 //   var geo_nodes = JSON.parse(data);
 //   for (var ind in geo_nodes.features) {
@@ -18,13 +38,7 @@ var fs = require("fs");
 //   }
 // });
 
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
-//
+// ROUTING
 // router.route('/buildings')
 //
 // // get example
@@ -64,33 +78,7 @@ router.get('/', function(req, res) {
 // });
 
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-app.use(express.static('client'));
-
-app.get('/*', function(req, res, next){
-  var url = req.url;
-  //Checking for urls like ../../passwd etc
-  if(!url.match(/\.\.+?\//)){
-    res.sendFile(req.url, {root:'./client'});
-  } else if(url === '/'){
-    next();
-  } else {
-    res.status(405).send('Did you try something naughty?');
-  }
-});
-
-app.get('/', function(req, res){
-  res.sendFile('index.html', {root:'./client'});
-});
-
-//Config checks process.env, otherwise sets to 9000
-var port = process.env.PORT || 9000;
-
-module.exports.listen = function(){
-  server.listen(port, function(){
-    console.log('Server listening on port', port);
-  });
-};
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+//router.get('/', function(req, res) {
+//  res.json({ message: 'hooray! welcome to our api!' });
+//});
