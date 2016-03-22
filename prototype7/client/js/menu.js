baaca15f1e4613bbec94f2adcee54cb24bf4277d
sudood <rizzo.mc9@gmail.com>
2016-03-22 17:46:00 -0400

    BI-436 0.5.6 - prototype7 menu

---
+++ prototype7/client/js/menu.js
@@ -0,0 +1,15 @@
+function home(){
+  window.location.href = 'http://127.0.0.1:9000';
+}
+function totCapita(){
+  window.location.href = 'http://127.0.0.1:9000/totCapita.html';
+}
+function totGreen(){
+  window.location.href = 'http://127.0.0.1:9000/totGreen.html';
+}
+function totTransit(){
+  window.location.href = 'http://127.0.0.1:9000/totTransit.html';
+}
+function totSocial(){
+  window.location.href = 'http://127.0.0.1:9000/totSocial.html';
+}
